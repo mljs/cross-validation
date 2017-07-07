831c9985452e8bff726677f5f9a7a3ac8ce10042
Daniel Kostro <kostro.d@gmail.com>
2017-07-07 16:19:21 +0200

    Modify tests slightly

--- test/crossValidation.js
+++ test/crossValidation.js
@@ -8,8 +8,8 @@ describe('basic', function () {
         var LOO = require('./data/LOO-CV');
         for (let i = 0; i < LOO.length; i++) {
             var CM = CV.leaveOneOut(Dummy, LOO[i].features, LOO[i].labels, LOO[i].classifierOptions);
-            CM.matrix.should.deepEqual(LOO[i].result.matrix);
-            CM.labels.should.deepEqual(LOO[i].result.labels);
+            CM.getMatrix().should.deepEqual(LOO[i].result.matrix);
+            CM.getLabels().should.deepEqual(LOO[i].result.labels);
         }
     });
 
@@ -17,8 +17,8 @@ describe('basic', function () {
         var LPO = require('./data/LPO-CV');
         for (let i = 0; i < LPO.length; i++) {
             var CM = CV.leavePOut(Dummy, LPO[i].features, LPO[i].labels, LPO[i].classifierOptions, LPO[i].p);
-            CM.matrix.should.deepEqual(LPO[i].result.matrix);
-            CM.labels.should.deepEqual(LPO[i].result.labels);
+            CM.getMatrix().should.deepEqual(LPO[i].result.matrix);
+            CM.getLabels().should.deepEqual(LPO[i].result.labels);
         }
     });
 
@@ -26,8 +26,8 @@ describe('basic', function () {
         var KF = require('./data/KF-CV');
         for (let i = 0; i < KF.length; i++) {
             var CM = CV.kFold(Dummy, KF[i].features, KF[i].labels, KF[i].classifierOptions, KF[i].k);
-            CM.matrix.should.deepEqual(KF[i].result.matrix);
-            CM.labels.should.deepEqual(KF[i].result.labels);
+            CM.getMatrix().should.deepEqual(KF[i].result.matrix);
+            CM.getLabels().should.deepEqual(KF[i].result.labels);
         }
     });
 });
