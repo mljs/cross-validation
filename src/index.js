51fe51d756f226cfa1a24cb14f5dcf885a040acf
Daniel Kostro <kostro.d@gmail.com>
2017-10-26 07:23:13 +0200

    feat: allow passing a callback to cross-validation methods

--- src/index.js
+++ src/index.js
@@ -6,16 +6,23 @@ const CV = {};
 const combinations = require('ml-combinations');
 
 /**
- * Performs a leave-one-out cross-validation (LOO-CV) of the given samples. In LOO-CV, 1 observation is used as the validation
- * set while the rest is used as the training set. This is repeated once for each observation. LOO-CV is a special case
- * of LPO-CV. @see leavePout
- * @param {function} Classifier - The classifier's constructor to use for the cross validation. Expect ml-classifier api.
+ * Performs a leave-one-out cross-validation (LOO-CV) of the given samples. In LOO-CV, 1 observation is used as the
+ * validation set while the rest is used as the training set. This is repeated once for each observation. LOO-CV is a
+ * special case of LPO-CV. @see leavePout
+ * @param {function} Classifier - The classifier's constructor to use for the cross validation. Expect ml-classifier
+ *     api.
  * @param {Array} features - The features for all samples of the data-set
  * @param {Array} labels - The classification class of all samples of the data-set
  * @param {object} classifierOptions - The classifier options with which the classifier should be instantiated.
  * @return {ConfusionMatrix} - The cross-validation confusion matrix
  */
 CV.leaveOneOut = function (Classifier, features, labels, classifierOptions) {
+    if (typeof labels === 'function') {
+        var callback = labels;
+        labels = features;
+        features = Classifier;
+        return CV.leavePOut(features, labels, 1, callback);
+    }
     return CV.leavePOut(Classifier, features, labels, classifierOptions, 1);
 };
 
@@ -25,7 +32,8 @@ CV.leaveOneOut = function (Classifier, features, labels, classifierOptions) {
  * validation set while the rest is used as the training set. This is repeated as many times as there are possible
  * ways to combine p observations from the set (unordered without replacement). Be aware that for relatively small
  * data-set size this can require a very large number of training and testing to do!
- * @param {function} Classifier - The classifier's constructor to use for the cross validation. Expect ml-classifier api.
+ * @param {function} Classifier - The classifier's constructor to use for the cross validation. Expect ml-classifier
+ *     api.
  * @param {Array} features - The features for all samples of the data-set
  * @param {Array} labels - The classification class of all samples of the data-set
  * @param {object} classifierOptions - The classifier options with which the classifier should be instantiated.
@@ -33,6 +41,12 @@ CV.leaveOneOut = function (Classifier, features, labels, classifierOptions) {
  * @return {ConfusionMatrix} - The cross-validation confusion matrix
  */
 CV.leavePOut = function (Classifier, features, labels, classifierOptions, p) {
+    if (typeof classifierOptions === 'function') {
+        var callback = classifierOptions;
+        p = labels;
+        labels = features;
+        features = Classifier;
+    }
     check(features, labels);
     const distinct = getDistinct(labels);
     const confusionMatrix = initMatrix(distinct.length, distinct.length);
@@ -50,7 +64,12 @@ CV.leavePOut = function (Classifier, features, labels, classifierOptions, p) {
             trainIdx.splice(testIdx[i], 1);
         }
 
-        validate(Classifier, features, labels, classifierOptions, testIdx, trainIdx, confusionMatrix, distinct);
+        if (callback) {
+            validateWithCallback(features, labels, testIdx, trainIdx, confusionMatrix, distinct, callback);
+        } else {
+            validate(Classifier, features, labels, classifierOptions, testIdx, trainIdx, confusionMatrix, distinct);
+        }
+
     }
 
     return new ConfusionMatrix(confusionMatrix, distinct);
@@ -68,6 +87,12 @@ CV.leavePOut = function (Classifier, features, labels, classifierOptions, p) {
  * @return {ConfusionMatrix} - The cross-validation confusion matrix
  */
 CV.kFold = function (Classifier, features, labels, classifierOptions, k) {
+    if (typeof classifierOptions === 'function') {
+        var callback = classifierOptions;
+        k = labels;
+        labels = features;
+        features = Classifier;
+    }
     check(features, labels);
     const distinct = getDistinct(labels);
     const confusionMatrix = initMatrix(distinct.length, distinct.length);
@@ -101,7 +126,11 @@ CV.kFold = function (Classifier, features, labels, classifierOptions, k) {
             if (j !== i) trainIdx = trainIdx.concat(folds[j]);
         }
 
-        validate(Classifier, features, labels, classifierOptions, testIdx, trainIdx, confusionMatrix, distinct);
+        if (callback) {
+            validateWithCallback(features, labels, testIdx, trainIdx, confusionMatrix, distinct, callback);
+        } else {
+            validate(Classifier, features, labels, classifierOptions, testIdx, trainIdx, confusionMatrix, distinct);
+        }
     }
 
     return new ConfusionMatrix(confusionMatrix, distinct);
@@ -126,18 +155,7 @@ function getDistinct(arr) {
 }
 
 function validate(Classifier, features, labels, classifierOptions, testIdx, trainIdx, confusionMatrix, distinct) {
-    var testFeatures = testIdx.map(function (index) {
-        return features[index];
-    });
-    var trainFeatures = trainIdx.map(function (index) {
-        return features[index];
-    });
-    var testLabels = testIdx.map(function (index) {
-        return labels[index];
-    });
-    var trainLabels = trainIdx.map(function (index) {
-        return labels[index];
-    });
+    const {testFeatures, trainFeatures, testLabels, trainLabels} = getTrainTest(features, labels, testIdx, trainIdx);
 
     var classifier;
     if (Classifier.prototype.train) {
@@ -148,9 +166,44 @@ function validate(Classifier, features, labels, classifierOptions, testIdx, trai
     }
 
     var predictedLabels = classifier.predict(testFeatures);
+    updateConfusionMatrix(confusionMatrix, testLabels, predictedLabels, distinct);
+}
+
+function validateWithCallback(features, labels, testIdx, trainIdx, confusionMatrix, distinct, callback) {
+    const {testFeatures, trainFeatures, testLabels, trainLabels} = getTrainTest(features, labels, testIdx, trainIdx);
+    const predictedLabels = callback(trainFeatures, trainLabels, testFeatures);
+    updateConfusionMatrix(confusionMatrix, testLabels, predictedLabels, distinct);
+}
+
+function updateConfusionMatrix(confusionMatrix, testLabels, predictedLabels, distinct) {
+
     for (var i = 0; i < predictedLabels.length; i++) {
-        confusionMatrix[distinct.indexOf(testLabels[i])][distinct.indexOf(predictedLabels[i])]++;
+        const actualIdx = distinct.indexOf(testLabels[i]);
+        const predictedIdx = distinct.indexOf(predictedLabels[i]);
+        if (actualIdx < 0 || predictedIdx < 0) {
+            // eslint-disable-next-line no-console
+            console.warn(`ignore unknown predicted label ${predictedLabels[i]}`);
+        }
+        confusionMatrix[actualIdx][predictedIdx]++;
     }
+}
+
+
+function getTrainTest(features, labels, testIdx, trainIdx) {
+    return {
+        testFeatures: testIdx.map(function (index) {
+            return features[index];
+        }),
+        trainFeatures: trainIdx.map(function (index) {
+            return features[index];
+        }),
+        testLabels: testIdx.map(function (index) {
+            return labels[index];
+        }),
+        trainLabels: trainIdx.map(function (index) {
+            return labels[index];
+        })
+    };
 }
 
 module.exports = CV;
